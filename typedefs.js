ca0c8e96d4cc9dd26156fe4e0f99b5d71e73229d
Kaushik Narayan R <rknarayan02@gmail.com>
2023-05-12 23:40:39 +0530

    utils

--- typedefs.js
+++ typedefs.js
@@ -7,9 +7,9 @@
  * @typedef {import("express").Response} Res
  * @typedef {import("express").NextFunction} Next
  * 
- * @typedef {import("sequelize") Sequelize}
- * @typedef {import("sequelize").Model Model}
- * @typedef {import("sequelize").QueryInterface QueryInterface}
+ * @typedef {import("sequelize")} Sequelize
+ * @typedef {import("sequelize").Model} Model
+ * @typedef {import("sequelize").QueryInterface} QueryInterface
  * 
  * @typedef {import("winston").Logger} Logger
  */
